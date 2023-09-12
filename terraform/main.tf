baf09e454cf1ee2c246e9c211d3698517c302c13
Moses Mbugua <swahili1963@gmail.com>
2023-09-11 22:21:50 -0700

    Add a vpc to the infra

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,21 +1,45 @@
-terraform {
-  backend "s3" {
-    bucket = "moger-terraform-state"
-    key = "terraform.tfstate"
-    region = "us-west-2"
-    encrypt = true
-  }
-
-  required_providers {
-    aws = {
-        source = "hashicorp/aws"
-        version = "~> 4.16"
-    }
-  }
-
-  required_version = ">= 1.2.0"
-}
 
 provider "aws" {
-    region = "us-west-2"
+  region = "us-west-2"
+}
+
+locals {
+  cluster_name = "moger-eks-${random_string.suffix.result}"
+}
+
+resource "random_string" "suffix" {
+  length = 8
+  special = false
+}
+
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+  version = "5.0.0"
+
+  name = "moger-vpc"
+
+  cidr = "10.0.0.0/16"
+  azs = ["us-west-2a", "us-west-2b", "us-west-2c", "us-west-3d"]
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  public_subnets  = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
+
+
+  enable_nat_gateway = true
+  single_nat_gateway = true
+  enable_dns_hostnames = true
+
+  public_subnet_tags = {
+    "kubernetes.io/cluster/${local.cluster_name}" = "shared"
+    "kubernetes.io/role/elb" = 1
+  }
+
+  private_subnet_tags = {
+    "kubernetes.io/cluster/${local.cluster_name}" = "shared"
+    "kubernetes.io/role/internal-elb" = 1
+  }
+
+  tags = {
+    Terraform = "true"
+    Environment = "dev"
+  }
 }
---
+++ terraform/terraform.tf
@@ -0,0 +1,22 @@
+terraform {
+  backend "s3" {
+    bucket = "moger-terraform-state"
+    key = "terraform.tfstate"
+    region = "us-west-2"
+    encrypt = true
+  }
+
+  required_providers {
+    aws = {
+        source = "hashicorp/aws"
+        version = "~> 5.1.0"
+    }
+
+    random = {
+        source = "hashicorp/random"
+        version = "~> 3.5.1"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
