e6c058f3fcc1fb32e8997db137397649bd5671d1
Moses Mbugua <swahili1963@gmail.com>
2023-09-16 22:37:06 -0700

    provision a eks cluster

--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -1,20 +1,20 @@
 terraform {
   backend "s3" {
-    bucket = "moger-terraform-state"
-    key = "terraform.tfstate"
-    region = "us-west-2"
+    bucket  = "moger-terraform-state"
+    key     = "terraform.tfstate"
+    region  = "us-west-2"
     encrypt = true
   }
 
   required_providers {
     aws = {
-        source = "hashicorp/aws"
-        version = "~> 5.1.0"
+      source  = "hashicorp/aws"
+      version = "~> 5.1.0"
     }
 
     random = {
-        source = "hashicorp/random"
-        version = "~> 3.5.1"
+      source  = "hashicorp/random"
+      version = "~> 3.5.1"
     }
   }
 
