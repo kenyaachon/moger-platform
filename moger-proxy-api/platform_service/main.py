e559f9080e8daa2a6e336903744ee72b3919c03c
Moses Mbugua <swahili1963@gmail.com>
2023-09-26 06:09:08 +0000

    add endpoints for scaling up and scaling down the eks cluster

--- moger-proxy-api/platform_service/main.py
+++ moger-proxy-api/platform_service/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.responses import JSONResponse
 from mangum import Mangum
 import logging
 import boto3
@@ -7,18 +8,17 @@ from botocore.config import Config
 app = FastAPI()
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger()
-
+my_config = Config(region_name = 'us-west-2')
+eks = boto3.client("eks", config=my_config)
 
 def get_eks_node_groups():
-    my_config = Config(region_name = 'us-west-2')
-    client = boto3.client("eks", config=my_config)
-    response = client.list_clusters(
+    response = eks.list_clusters(
         maxResults=23,
     )    
     cluster_name = response['clusters'][0]
     logger.info(f"our cluster is {cluster_name}")
 
-    response = client.list_nodegroups(clusterName=cluster_name, maxResults=23)
+    response = eks.list_nodegroups(clusterName=cluster_name, maxResults=23)
 
     logger.info(f"here is our node groups {response['nodegroups']}")
 
@@ -26,15 +26,13 @@ def get_eks_node_groups():
     return cluster_name, node_groups
 
 def get_eks_cluster():
-    my_config = Config(region_name = 'us-west-2')
-    client = boto3.client("eks", config=my_config)
-    response = client.list_clusters(
+    response = eks.list_clusters(
         maxResults=23,
     )    
     cluster_name = response['clusters'][0]
     logger.info(f"our cluster is {cluster_name}")
 
-    response = client.list_nodegroups(clusterName=cluster_name, maxResults=23)
+    response = eks.list_nodegroups(clusterName=cluster_name, maxResults=23)
 
     logger.info(f"here is our node groups {response['nodegroups']}")
 
@@ -43,7 +41,7 @@ def get_eks_cluster():
     nodegroup_minsizes = {}
     nodegroup_maxsizes = {}
     for node in node_groups:
-        response = client.describe_nodegroup(clusterName=cluster_name, nodegroupName=node)
+        response = eks.describe_nodegroup(clusterName=cluster_name, nodegroupName=node)
         scaling_config = response['nodegroup']['scalingConfig']
         # logger.info(f"here is the scaling config {scaling_config} for node {node}")
         nodegroup_sizes[node] = scaling_config['desiredSize']
@@ -54,7 +52,7 @@ def get_eks_cluster():
     logger.info(f"here is the nodegroup_maxsizes {nodegroup_maxsizes}")
     logger.info(f"here is the nodegroup_minsizes {nodegroup_minsizes}")
 
-    return nodegroup_sizes, nodegroup_minsizes, nodegroup_maxsizes
+    return cluster_name, nodegroup_sizes, nodegroup_minsizes, nodegroup_maxsizes
 
 @app.get("/")
 async def greeting_world():
@@ -62,6 +60,99 @@ async def greeting_world():
     get_eks_cluster()
 
     return {"message": "hello world"}
+
+@app.post("/eks/scale-up")
+async def scale_up_eks_cluster():
+    cluster_name, nodegroup_sizes, nodegroup_minsizes, nodegroup_maxsizes = get_eks_cluster()
+
+    for nodegroup_name, max_size in nodegroup_maxsizes.items():
+        response = eks.describe_nodegroup(
+            clusterName=cluster_name,
+            nodegroupName=nodegroup_name
+        )
+        current_size = response['nodegroup']['scalingConfig']['maxSize']
+
+        if max_size != current_size:
+            response = eks.update_nodegroup_config(
+                clusterName=cluster_name,
+                nodegroupName=nodegroup_name,
+                scalingConfig={
+                    'maxSize': max_size
+                }
+            )
+            print(
+                f"Updated maximum size for node group {nodegroup_name} to {max_size}")
+        else:
+            print(
+                f"Maximum size is already {max_size} for node group {nodegroup_name}")
+
+    for nodegroup_name, desired_size in nodegroup_sizes.items():
+        response = eks.describe_nodegroup(
+            clusterName=cluster_name,
+            nodegroupName=nodegroup_name
+        )
+        current_size = response['nodegroup']['scalingConfig']['desiredSize']
+
+        if desired_size != current_size:
+            response = eks.update_nodegroup_config(
+                clusterName=cluster_name,
+                nodegroupName=nodegroup_name,
+                scalingConfig={
+                    'desiredSize': desired_size
+                }
+            )
+            print(
+                f"Updated desired size for node group {nodegroup_name} to {desired_size}")
+        else:
+            print(
+                f"Desired size is already {desired_size} for node group {nodegroup_name}")
+
+    for nodegroup_name, min_size in nodegroup_minsizes.items():
+        response = eks.describe_nodegroup(
+            clusterName=cluster_name,
+            nodegroupName=nodegroup_name
+        )
+        current_size = response['nodegroup']['scalingConfig']['minSize']
+
+        if min_size != current_size:
+            response = eks.update_nodegroup_config(
+                clusterName=cluster_name,
+                nodegroupName=nodegroup_name,
+                scalingConfig={
+                    'minSize': min_size
+                }
+            )
+            print(
+                f"Updated minimal size for node group {nodegroup_name} to {min_size}")
+        else:
+            print(
+                f"Minimal size is already {min_size} for node group {nodegroup_name}")
+    return JSONResponse(status_code=200, content={"message": "scaling up eks cluster"})
+
+@app.post("/eks/scale-down")
+async def scale_down_eks_cluster():
+    cluster_name, nodegroup_names = get_eks_node_groups()
+    
+    new_desiredSize = 0
+    new_minSize = 0
+    new_maxSize = 1
+
+    # Loop through the node groups and update their desired capacity to 0
+    for nodegroup_name in nodegroup_names:
+        response = eks.update_nodegroup_config(
+            clusterName=cluster_name,
+            nodegroupName=nodegroup_name,
+            scalingConfig={
+                "desiredSize": new_desiredSize,
+                "minSize": new_minSize,
+                "maxSize": new_maxSize
+            }
+        )
+
+        # Print the response
+        logger.info(response)
+
+    return JSONResponse(status_code=200, content={"message": "scaling down eks cluster"})
 
 handler = Mangum(app)
 
