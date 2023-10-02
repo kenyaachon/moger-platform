04f75ccaf9452028d8362aca93a7a9e87e75977f
Moses Mbugua <swahili1963@gmail.com>
2023-10-01 22:33:18 -0700

    improvement for redirecting to original page after login

--- moger-admin/src/views/LoginView.vue
+++ moger-admin/src/views/LoginView.vue
@@ -1,18 +1,29 @@
 <script setup lang="ts">
 import { Authenticator } from '@aws-amplify/ui-vue'
 import { Auth, Hub, Amplify } from 'aws-amplify'
+import { useRoute } from 'vue-router'
+
 import '@aws-amplify/ui-vue/styles.css'
 // import awsconfig from '@/aws-exports'
 import awsmobile from '@/aws-exports'
+import router from '../router'
+
+const route = useRoute()
 
 // Amplify.configure(awsconfig)
 Amplify.configure(awsmobile)
+
 Hub.listen('auth', async ({ payload }) => {
   console.log(payload)
   try {
     const data = await Auth.currentSession()
     console.log(data)
     console.log(`here is the access token${data.getAccessToken()}`)
+    if (payload.event === 'signIn') {
+      console.log(`here is the routes ${route.query.redirect}`)
+      const origin = route.query.redirect?.toString() as string
+      router.push(origin)
+    }
   } catch (err) {
     console.log(err)
   }
